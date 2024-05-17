00139102eaf47fd82232a3601d07017f9d0d8e52
¨Sebastian <laresxd@outlook.com>
2024-05-16 23:19:55 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { NavBar } from './components/ItemListContainer'
+import { ItemListContainer } from './components/NavBar'
+import './css/main.css'
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <ItemListContainer />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,17 @@
+import { Carrito } from "./Carrito"
+
+export const NavBar = () => {
+    return (
+        <header>
+            <h1>Salon de Masajes MB</h1>
+            <nav>
+                <ul>
+                    <li><a href="#"></a></li>
+                    <li><a href="#"></a></li>
+                    <li><a href="#"></a></li>
+                </ul>
+            </nav>
+            <Carrito />
+        </header>
+    )
+} 
